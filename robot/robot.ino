e812076f62db66f5fa0016bd2fdf3c37bef74ce3
Tomasz Moczek <t.moczek@gmail.com>
2023-05-07 14:58:35 +0200

    changes added

---
+++ robot/robot.ino
@@ -0,0 +1,70 @@
+int trig = 13;
+int echo = 12;
+int czas;
+int odleglosc;
+
+int enablePin1 = 11;
+int input1A = 10;
+int input2A = 7;
+
+int enablePin2 = 6;
+int input1B = 4;
+int input2B = 5;
+
+void setup() {
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(9600);
+
+  pinMode(enablePin1, OUTPUT);
+  pinMode(input1A, OUTPUT);
+  pinMode(input2A, OUTPUT);
+  digitalWrite(enablePin1, LOW);
+  
+  pinMode(enablePin2, OUTPUT);
+  pinMode(input1B, OUTPUT);
+  pinMode(input2B, OUTPUT);
+  digitalWrite(enablePin2, LOW);
+}
+
+void loop() {
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trig, LOW);
+  czas = pulseIn(echo, HIGH);
+  odleglosc = (czas / 2) / 29.1;
+  Serial.print(odleglosc);
+  Serial.print(" cm");
+  Serial.println();
+  delay (20);
+  
+  digitalWrite(input1A, HIGH);
+  digitalWrite(input2A, LOW);
+  analogWrite(enablePin1, 255);
+  
+  digitalWrite(input1B, HIGH);
+  digitalWrite(input2B, LOW);
+  analogWrite(enablePin2, 255);
+  
+  if (odleglosc > 40 ) {
+    digitalWrite(input1A, HIGH);
+    digitalWrite(input2A, LOW);
+    analogWrite(enablePin1, 255);
+    
+    digitalWrite(input1B, HIGH);
+    digitalWrite(input2B, LOW);
+    analogWrite(enablePin2, 255);
+  }
+  else {
+    digitalWrite(enablePin1, LOW);
+    delay(500);
+    
+    digitalWrite(input1A, LOW);
+    digitalWrite(input2A, HIGH);
+    analogWrite(enablePin1, 255);
+    
+    digitalWrite(input1B, HIGH);
+    digitalWrite(input2B, LOW);
+    analogWrite(enablePin2, 255);
+  }
+}
